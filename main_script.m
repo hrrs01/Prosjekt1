38efed95c15adceaa50545b750bb5c0e8b64991a
hrrs01 <haavard.hn@gmail.com>
2024-01-11 09:39:48 +0100

    Nå funka sensoren

--- main_script.m
+++ main_script.m
@@ -1,3 +1,5 @@
-t = 1 : 1 : 740;
-data = imp.Data(:);
-plot(t, data)
+tilfort_spenning = 12.22; % Volt, målt med multimeter
+motstand = 25; % Ohm
+malt_motstand = 26.3; % Ohm
+thermal_resistance_motstand = 3.3; % C/W
+junction_to_ambient_lm35 = 90; % C/W
